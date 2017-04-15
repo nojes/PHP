95c3ef51493da910fcc6852d2f33d9fe30fed04e
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2017-04-15 12:34:33 +0300

    cycle `for`

--- lesson2/index.php
+++ lesson2/index.php
@@ -8,9 +8,26 @@ $b = 'value';
 $c = "$a $b";
 
 if ($_GET['access'] == 1) {
-    echo "hello";
+    echo 'hello';
 } else {
-    echo "Access denied";
+    echo 'Access denied';
 }
 
-echo $c;
+echo $c . '<br><br>';
+?>
+
+<?
+/**
+ * Simple multiplication table example
+ */
+$size = 11;
+?>
+<table>
+    <?php for ($i = 1; $i <= $size; $i++): ?>
+        <tr>
+            <?php for ($j = 1; $j <= $size; $j++): ?>
+                <td><?= $j * $i ?></td>
+            <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+</table>
